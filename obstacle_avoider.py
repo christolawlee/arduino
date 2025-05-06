1cb0c5d82bbd6c5ad0cb5d8aab6ef8a94fb08c86
Christopher Lee <christopherlawlee@icloud.com>
2025-05-06 11:53:03 -0700

    Update obstacle_avoider.py

--- obstacle_avoider.py
+++ obstacle_avoider.py
@@ -12,8 +12,8 @@ safe_distance_inner = 10
 safe_distance_center = 5
 speed = 100.00        # Movement speed
 reverse_distance = -5.00
-rotation_angle = 90  # Angle to rotate when avoiding obstacles
-pause_time = 10
+rotation_angle = 180  # Angle to rotate when avoiding obstacles
+pause_time = 1000
 last_obstacle = 1
 
 while True:
